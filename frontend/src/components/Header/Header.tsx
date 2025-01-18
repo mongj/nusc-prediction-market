7a8232710472164173937f74b4a0f87ec3e6555c
ywllowsensor <isyazani@gmail.com>
2025-01-18 22:35:29 +0800

    Convert all css to tailwind css

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -6,9 +6,9 @@ import Button from '../../components/Buttons/Button/Button';
 const Header = () => {
     const navigate = useNavigate();
     return (
-        <div className="flex w-full justify-between items-center">
-            <h1 className="text-4xl font-bold">NUSC Prediction Market Study</h1>
-            <div className='flex-row flex space-x-4'>
+        <div className="flex flex-col sm:flex-row w-full justify-between items-center p-4">
+            <h1 className="text-2xl sm:text-4xl font-bold">NUSC Prediction Market Study</h1>
+            <div className="flex space-x-2 mt-2 sm:mt-0">
                 <HelpButton2 text="Help" onClick={() => alert('Help button clicked!')} />
                 <Button text="Contact Us" onClick={() => navigate('/dashboard/contactus')} />
                 <SignOutButton text="Sign Out" onClick={() => alert('Sign Out button clicked!')} />
