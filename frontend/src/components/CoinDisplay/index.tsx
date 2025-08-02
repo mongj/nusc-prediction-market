c19c5c145ad527310297d30ff0d4dc467f4b89af
ywllowsensor <isyazani@gmail.com>
2025-08-03 01:12:03 +0800

    Update phrasing

--- frontend/src/components/CoinDisplay/index.tsx
+++ frontend/src/components/CoinDisplay/index.tsx
@@ -74,8 +74,7 @@ const CoinDisplay = () => {
         }}
       >
         <div className="p-3 space-y-2 text-sm font-normal">
-          <p className="text-gray-800 leading-relaxed">This is the total amount of coins you have earned.</p>
-          <p className="text-gray-800 leading-relaxed">You can use these coins to make your predictions.</p>
+          <p className="text-gray-800 leading-relaxed">This is the total amount of coins you have earned. Use these coins to make your predictions!</p>
           <p className="text-gray-800 leading-relaxed">Your coins will be converted to real currency at the end of this study at a rate of 15c per coin.</p>
         </div>
       </Popover>
